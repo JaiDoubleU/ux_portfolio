e522c6a2675c357f5542a5c5c72464730141ada7
sarthak <sarthak.ahuja0007@gmail.com>
2021-07-09 19:57:30 +0530

    firebase intit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDyeNvQEA9LlVmTM29Le4U1cfvpfv1srok",
+    authDomain: "portfolio-e2be9.firebaseapp.com",
+    projectId: "portfolio-e2be9",
+    storageBucket: "portfolio-e2be9.appspot.com",
+    messagingSenderId: "158979380547",
+    appId: "1:158979380547:web:252146d27ac386e0e48bf6",
+    measurementId: "G-1CX3V67D3K"
+};
+firebase.initializeApp(firebaseConfig);
