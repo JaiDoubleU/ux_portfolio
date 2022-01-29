693d26a82db293f1c438f9b3730f28dbd0cb9f6a
sarthak <sarthak.ahuja0007@gmail.com>
2022-01-29 16:58:47 +0530

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom'
+import RouteChangeTracker from './RouteChangeTracker';
+
+// GOOGLE ANALYTICS
+
+import ReactGA from 'react-ga'
+
+const TRACKING_ID = "UA-12341234-1";
+ReactGA.initialize(TRACKING_ID);
+
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div >
-        <Main />
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div >
+                <Main />
+                <RouteChangeTracker />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
