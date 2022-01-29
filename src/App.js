a92c485f5f30490d6a2c5a6f399dcbe49ef401db
sarthak <sarthak.ahuja0007@gmail.com>
2022-01-29 22:24:40 +0530

    react GA

--- src/App.js
+++ src/App.js
@@ -4,19 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import RouteChangeTracker from './RouteChangeTracker';
 import ReactGA from 'react-ga'
 
-// GOOGLE ANALYTICS
-
-useEffect(() => {
-    const TRACKING_ID = process.env.GA_ID;
-    ReactGA.initialize(TRACKING_ID);
-    ReactGA.pageview(window.location.pathname + window.location.search);
-}, [])
-
-
-
-
 
 function App() {
+    useEffect(() => {
+        const TRACKING_ID = process.env.GA_ID;
+        ReactGA.initialize(TRACKING_ID);
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, [])
     return (
         <BrowserRouter>
             <div >
